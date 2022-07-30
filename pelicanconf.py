d3e1d88609432397d4ae6b2112fd81c58fbd3509
Tadej Borovšak <70951+tadeboro@users.noreply.github.com>
2022-07-30 17:47:58 +0200

    Test GH actions

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+AUTHOR = "Tadej Borovšak"
+SITENAME = "Little silly things"
+SITEURL = ""
+
+PATH = "content"
+THEME = "theme"
+
+TIMEZONE = "UTC"
+DEFAULT_DATE_FORMAT = "%Y-%m-%d"
+
+SLUGIFY_SOURCE = "basename"
+
+DIRECT_TEMPLATES = ["index", "tags"]
+TAGS_SAVE_AS = "tags/index.html"
+for i in ("article", "tag"):
+    globals()[f"{i.upper()}_URL"] = i + "s/{slug}/"
+    globals()[f"{i.upper()}_SAVE_AS"] = i + "s/{slug}/index.html"
+for i in ("category", "page", "author"):
+    globals()[f"{i.upper()}_SAVE_AS"] = ""
+
+DEFAULT_LANG = "en"
+
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = False
---
+++ publishconf.py
@@ -0,0 +1,12 @@
+import os
+import sys
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+SITEURL = 'https://tadej.borovsak.si'
+RELATIVE_URLS = False
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+
+DELETE_OUTPUT_DIRECTORY = True
